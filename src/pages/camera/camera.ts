749254a74e85585da74ab3b94202c2cd5c9b3638
Giuan Adauto <giuanadauto@hotmail.com>
2017-11-28 15:10:15 -0300

    camera added

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import {Camera} from 'ionic-native';
+
 /**
  * Generated class for the CameraPage page.
  *
@@ -14,12 +16,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'camera.html',
 })
 export class CameraPage {
+  base64Image: any = "empty";
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CameraPage');
+  }
+
+  takePicture(){
+    Camera.getPicture({
+        destinationType: Camera.DestinationType.DATA_URL,
+        targetWidth: 1000,
+        targetHeight: 1000
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+        console.log(err);
+    });
   }
 
 }
